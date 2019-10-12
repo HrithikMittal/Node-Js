d1fb273921467dd700c5f7f1f6683abcd762cb98
HrithikMittal <adhikanshmittal@gmail.com>
2019-10-12 11:33:52 +0530

    starts with the basics

---
+++ node-basic/app.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+// Simple Function in JS
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(4, 5));
+
+// Arrow Function in JS
+var sub = (a, b) => {
+  return a - b;
+};
+console.log(sub(5, 4));
