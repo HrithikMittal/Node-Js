855d5c9e21a6991d95d3a617f40b4a32b6878e08
HrithikMittal <adhikanshmittal@gmail.com>
2019-10-12 18:35:21 +0530

    middleware in node js

--- nodeapi/index.js
+++ nodeapi/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 var app = express();
+const morgan = require("morgan");
+
+// bring in routes
+const { getPosts } = require("./routes/post");
+
+// middleware
+const myOwnMiddleware = (req, res, next) => {
+  console.log("this is mymiddleware");
+  next();
+};
+
+app.use(morgan("dev"));
+app.use(myOwnMiddleware);
+
+app.get("/", getPosts);
 
 var port = 3000;
-
-app.get("/", (req, res) => {
-  res.send(`Hi I am Adhikansh`);
-});
-
 app.listen(port, () => {
   console.log("Server is listening");
 });
---
+++ nodeapi/routes/post.js
@@ -0,0 +1,6 @@
+const getPosts = (req, res) => {
+  res.send(`Hi I am Adhikansh`);
+};
+module.exports = {
+  getPosts
+};
