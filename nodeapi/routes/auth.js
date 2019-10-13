16cba3769b695c6ec51593d9f10aa7f324c11acd
HrithikMittal <adhikanshmittal@gmail.com>
2019-10-13 10:35:40 +0530

    create the signup validator

--- nodeapi/routes/auth.js
+++ nodeapi/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 var router = express.Router();
 const auth = require("../controllers/auth");
+const validator = require("../validator/index");
 
-router.post("/signup", auth.signup);
+router.post("/signup", validator.userSignupValidator, auth.signup);
 
 module.exports = router;
--- nodeapi/validator/index.js
+++ nodeapi/validator/index.js
@@ -23,3 +23,49 @@ exports.createPostValidator = (req, res, next) => {
   // proceed to next middleware
   next();
 };
+
+exports.userSignupValidator = (req, res, next) => {
+  // name is not null and between 4-10 characters
+  req.check("name", "Name is required").notEmpty();
+  req
+    .check("name", "Name length should be between 4 to 10 characters")
+    .isLength({
+      min: 4,
+      max: 10
+    });
+
+  //email is not null, valid and normalized
+  req.check("email", "Email is required").notEmpty();
+  req
+    .check("email", "Email length should be between 3 to 32 characters")
+    .isLength({
+      min: 4,
+      max: 32
+    });
+  req
+    .check("email", "Email should be proper")
+    .matches(/.+\@.+\..+/)
+    .withMessage("Email must contain @");
+
+  //check for password
+  req.check("password", "Password is required").notEmpty();
+  req
+    .check("password", "Password should be minimum 6 length")
+    .isLength({
+      min: 6
+    })
+    .matches(/\d/)
+    .withMessage("Password Must contain a number");
+
+  //check for errors
+  const errors = req.validationErrors();
+
+  // If error show the first one as they happen
+  if (errors) {
+    const firstError = errors.map(error => error.msg)[0];
+    return res.status(400).json({ error: firstError });
+  }
+
+  // proceed to next middleware
+  next();
+};
