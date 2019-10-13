fa8c2fa080d113527c82675890a9d5448d30238b
HrithikMittal <adhikanshmittal@gmail.com>
2019-10-13 09:47:11 +0530

    encrypt password using crpto

--- nodeapi/modals/user.js
+++ nodeapi/modals/user.js
@@ -1,4 +1,7 @@
+const uuidv1 = require("uuid/v1");
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+
 var userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -21,5 +24,42 @@ var userSchema = new mongoose.Schema({
   },
   updated: Date
 });
+
+/**
+ * Virtual fiels are additional fields for a given model.
+ * Their values can be set manually or automatically with defined functionality.
+ * keep in mind: virual properties{password} don't get persisted in the database.
+ * They only exist logically and are not written to the document's collection
+ */
+
+// Virtual field
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    // create temporary variable called _password
+    this._password = password;
+    // generate a time stamp
+    this.salt = uuidv1();
+    // encrypt the password
+    this.hashedpassword = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+// methods
+userSchema.methods = {
+  encryptPassword: function(password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  }
+};
 
 module.exorts = mongoose.model("User", userSchema);
